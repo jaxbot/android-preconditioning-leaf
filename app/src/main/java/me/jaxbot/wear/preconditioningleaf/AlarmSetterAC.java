1309e8043cfc99e8c4287e8d542047b14ae505ba
Jonathan Warner <jaxbot@gmail.com>
2015-01-13 18:46:39 -0500

    Don't overwrite previous requestCodes

--- app/src/main/java/me/jaxbot/wear/preconditioningleaf/AlarmSetterAC.java
+++ app/src/main/java/me/jaxbot/wear/preconditioningleaf/AlarmSetterAC.java
@@ -4,6 +4,7 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -12,12 +13,15 @@ import java.util.Calendar;
  * Created by jonathan on 1/10/15.
  */
 public class AlarmSetterAC {
+    final static String TAG = "AlarmSetterAC";
     final static int REQUEST_SET_ALARMS = 1;
-    public static void setAlarm(Context context, long startTime, String eventName)
+    final static int REQUEST_ACSTART_BOUNDARY = 10;
+
+    public static void setAlarm(Context context, long startTime, String eventName, int i)
     {
         Intent intent = new Intent(context, AlarmReceiverAC.class);
         intent.putExtra("eventName", eventName);
-        PendingIntent sender = PendingIntent.getBroadcast(context, 4, intent, 0);
+        PendingIntent sender = PendingIntent.getBroadcast(context, REQUEST_ACSTART_BOUNDARY + i, intent, 0);
         AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         am.set(AlarmManager.RTC_WAKEUP, startTime, sender);
     }
@@ -26,11 +30,17 @@ public class AlarmSetterAC {
         ArrayList<ScheduledStart> events = CalendarSchedule.getStartEndTimes(ctx);
         String timesStr = "";
         Long currentTime = System.currentTimeMillis();
+
+        int i = 0;
         for (ScheduledStart event : events) {
             if (event.time > currentTime) {
                 timesStr += event.name + ": " + CalendarSchedule.longToDate(event.time) + "\n";
-                AlarmSetterAC.setAlarm(ctx, event.time, event.name);
+                AlarmSetterAC.setAlarm(ctx, event.time, event.name, i);
+                Log.i(TAG, "Setting: " + CalendarSchedule.longToDate(event.time) + " - " + event.name);
+            } else {
+                Log.i(TAG, "Not setting: " + CalendarSchedule.longToDate(event.time) + " - " + event.name);
             }
+            i++;
         }
         System.out.println(timesStr);
     }
